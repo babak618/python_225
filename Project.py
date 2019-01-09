456e5791a08b87bd1dfbc5fa60c44b97d0d8a30e
babak618 <babak.office@gmail.com>
2019-01-09 19:55:53 +0330

    find row column,

--- Project.py
+++ Project.py
@@ -4,5 +4,9 @@ with open('sample.csv') as csv_file:
     list1 = [row for row in csv_read]
     #print(list1)
     list2 = [column[5] for column in list1]
-    print(list2)
+    #print(list2)
     print(max(list2))
+    line = list2.index(max(list2))
+    #print(line)
+    print(list1[line])
+
