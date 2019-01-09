660a23d170308316329dc4eb61340b65d31d95c8
babak618 <babak.office@gmail.com>
2019-01-09 19:31:17 +0330

    make list & print column.

--- Project.py
+++ Project.py
@@ -1,5 +1,8 @@
 import csv
 with open('sample.csv') as csv_file:
     csv_read = csv.reader(csv_file)
-    for i in csv_read:
-        print(i)
+    list1 = [row for row in csv_read]
+    #print(list1)
+    list2 = [column[5] for column in list1]
+    print(list2)
+    print(max(list2))
