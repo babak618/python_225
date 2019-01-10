b349535362f00129ec9b6cf32e7c41adc09d2f9a
babak618 <babak.office@gmail.com>
2019-01-10 23:07:41 +0330

    solved headline problem, unprofessional!!!

--- Project.py
+++ Project.py
@@ -1,12 +1,16 @@
 import csv
-with open('SAIPA-co.csv') as csv_file:
+with open('SAIPA-Co.csv') as csv_file:
     csv_read = csv.reader(csv_file)
     list1 = [row for row in csv_read]
     #print(list1)
-    list2 = [column[5] for column in list1]
+    list2 = []
+    for i in list1:
+        if '<OPEN>' not in i:
+            list2.append(i)
     #print(list2)
-    print(max(list2))
-    line = list2.index(max(list2))
+    list3 = [column[5] for column in list2]
+    print(max(list3))
+    line = list3.index(max(list3))
     #print(line)
-    print(list1[line])
+    print(list2[line])
 
