9d0a65cfe3f425796678519410b0eaddd516abc4
babak618 <babak.office@gmail.com>
2019-01-10 23:14:58 +0330

    Syntax enhancement,

--- Project.py
+++ Project.py
@@ -3,10 +3,7 @@ with open('SAIPA-Co.csv') as csv_file:
     csv_read = csv.reader(csv_file)
     list1 = [row for row in csv_read]
     #print(list1)
-    list2 = []
-    for i in list1:
-        if '<OPEN>' not in i:
-            list2.append(i)
+    list2 = [i for i in list1 if '<OPEN>' not in i]
     #print(list2)
     list3 = [column[5] for column in list2]
     print(max(list3))
