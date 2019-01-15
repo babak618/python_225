990345d6df707205fe5579a854473c024f7034ca
babak618 <babak.office@gmail.com>
2019-01-15 16:10:04 +0330

    candle structure, step 1

--- Project.py
+++ Project.py
@@ -9,3 +9,10 @@ with open('SAIPA-Co.csv') as csv_file:
             final_list.append(list1)
 
 print(final_list)
+# <date>[0],<OPEN>[1],<HIGH>[2],<LOW>[3],<CLOSE>[4],<VOL>[5]
+def morning_star_candle(list):
+    for i in list:
+        date,open,close,high,low,vol = i[0],i[1],i[4],i[2],i[3],i[5]
+        print(date, close)
+
+morning_star_candle(final_list)
